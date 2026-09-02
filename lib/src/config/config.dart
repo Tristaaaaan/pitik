3a6ba70b2f907883d3622599bc708ed60c55dc7e
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2026-09-02 14:34:30 +0800

    chore: app-setup

---
+++ lib/src/config/config.dart
@@ -0,0 +1,17 @@
+import 'config_enum.dart';
+
+class AppConfig {
+  static Flavors flavors = Flavors.production;
+  static Flavors get environment => flavors;
+
+  static void setEnvironment(Flavors environment) {
+    switch (environment) {
+      case Flavors.development:
+        flavors = Flavors.development;
+        break;
+      case Flavors.production:
+        flavors = Flavors.production;
+        break;
+    }
+  }
+}
