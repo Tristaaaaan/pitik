23492bf771fbe5fa4eda526e05235c5fad779e24
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2026-09-02 16:10:46 +0800

    chore: macOS flavors setup

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pitik/src/config/config.dart';
+import 'package:pitik/src/config/config_enum.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppConfig.setEnvironment(Flavors.development);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World Development!'))),
+    );
+  }
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pitik/src/config/config.dart';
+import 'package:pitik/src/config/config_enum.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppConfig.setEnvironment(Flavors.production);
   runApp(const MainApp());
 }
 
@@ -10,7 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(body: Center(child: Text('Hello World Production!'))),
     );
   }
 }
