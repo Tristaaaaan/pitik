9a62d5b6bebeb468048ba8897be8321130fbcad1
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2026-09-03 21:19:29 +0800

    chore: set max width and height

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'src/config/config.dart';
 import 'src/config/config_enum.dart';
@@ -6,6 +9,22 @@ import 'src/config/config_enum.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   AppConfig.setEnvironment(Flavors.development);
+
+  if (Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+
+    const windowOptions = WindowOptions(
+      minimumSize: Size(1100, 800),
+      size: Size(1100, 800),
+      center: true,
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
   runApp(const MainApp());
 }
 
